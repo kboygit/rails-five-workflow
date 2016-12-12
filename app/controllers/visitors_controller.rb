bffadfb7e7f3de6edaabedd90a41b6be0ae56682
kboygit <heybisayangdako@gmail.com>
2016-12-12 14:49:27 -0800

    testing flash messages on controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
 			render 'visitors/new'
+			flash[:notice] = 'Welcome'
+			flash[:alert] = 'My birthday is soon'
 	end
 
 
