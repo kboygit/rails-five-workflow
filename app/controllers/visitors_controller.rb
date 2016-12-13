3a4419818036b21bf3edd1d29324e3f5b2e56127
kboygit <heybisayangdako@gmail.com>
2016-12-13 12:00:27 -0800

    removed flash messages

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,9 +3,7 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
 			render 'visitors/new'
-			flash.now[:notice] = 'Welcome'
-			flash.now[:alert] = 'Warning'
-
+	
 	end
 
 
