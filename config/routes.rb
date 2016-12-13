99706813e87dab52f77571417533dc47679df9d6
kboygit <heybisayangdako@gmail.com>
2016-12-13 13:40:40 -0800

    configured routes for contact,added content in contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to:'visitors#new'
+  post 'contact', to: 'contacts#process_form'
+  root to:'visitors#new'
 end
